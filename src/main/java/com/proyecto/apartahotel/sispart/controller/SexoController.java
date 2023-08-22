5685e35e6d47a014bb4cc0099d92eb3f82ccb37b
Alejo <camiloalejandroahumadagomez@gmail.com>
2023-08-22 16:43:38 -0500

    camilo:aumento de tiempo de token

--- src/main/java/com/proyecto/apartahotel/sispart/controller/SexoController.java
+++ src/main/java/com/proyecto/apartahotel/sispart/controller/SexoController.java
@@ -1,4 +1,4 @@
-package com.proyecto.apartahotel.sispart.genero.controller;
+package com.proyecto.apartahotel.sispart.controller;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.proyecto.apartahotel.sispart.genero.entity.Sexo;
-import com.proyecto.apartahotel.sispart.genero.service.ISexoService;
-import com.proyecto.apartahotel.sispart.login.entity.UsuarioEmpleado;
+import com.proyecto.apartahotel.sispart.entity.Sexo;
+import com.proyecto.apartahotel.sispart.entity.UsuarioEmpleado;
+import com.proyecto.apartahotel.sispart.service.ISexoService;
 
 @RestController
 @RequestMapping("/sexo")
